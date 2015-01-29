ef1a91a7226834d60719d0b67f11f33b675441d5
xcsoft <starbish@xcsoft.pusku.com>
2015-01-29 20:58:00 +0900

    11111111

--- [xcsoft] SharpShooter/Champions/Vayne.cs
+++ [xcsoft] SharpShooter/Champions/Vayne.cs
@@ -18,7 +18,7 @@ namespace Sharpshooter.Champions
         public static void Load()
         {
             Q = new Spell(SpellSlot.Q, 900f);
-            E = new Spell(SpellSlot.E, Orbwalking.GetRealAutoAttackRange(Player));
+            E = new Spell(SpellSlot.E, 595f);
 
             E.SetTargetted(0.25f, 2200f);
 
